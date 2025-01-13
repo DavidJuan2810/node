502eb9c7fecf0805478bb056e5ce460789671a55
David <Juandavides120@gmail.com>
2025-01-12 22:01:49 -0500

    Creacion de los siguientes controladores verLote, actualizarLote, eliminarLote, con esto terminamos el crud completo para Lotes con su respectiva ruta

--- src/controllers/controller.lotes.js
+++ src/controllers/controller.lotes.js
@@ -16,6 +16,22 @@ export const listarLotes = async (req,res)=>{
     }
 }
 
+export const verLote = async (req,res) => {
+    try{
+        const id = req.params.id_lote;
+        const sql = " SELECT * FROM Lotes WHERE id_lote = ?";
+        const [result] = await pool.query(sql, [id]);
+
+        if (result.length > 0){
+            return res.status(200).json({msg: "lotes:", result});
+        }
+        return res.status(404).json({"message":"No se encontro el lote"});
+    } catch (error){
+        console.error(error);
+        return res.status(500).json({"message":"Error en el servidor"});
+    }
+}
+
 export const registrarLote = async (req,res)=>{
     try{
         const {nombre, descripcion, activo, TamX, TamY, posY, posX}=req.body
@@ -30,5 +46,48 @@ export const registrarLote = async (req,res)=>{
     }catch(error){
         console.error(error)
         return res.status(500).json({"message":"Error en el servidor"})
+    }
+}
+
+export const actualizarLote = async (req, res) => {
+    try{
+        const {nombre, descripcion, activo, TamX, TamY, posY, posX}=req.body
+        const id = req.params.id_lote;
+
+        if (!id) {
+            return res.status(400).json({"message":"no se encontro un id de lote valido"});
+        }
+        const sql = `UPDATE Lotes SET nombre = ?, descripcion = ?, activo = ?, TamX = ?, TamY = ?, posY =?, posX =?
+        WHERE id_lote = ?`;
+        const [rows] = await pool.query(sql, [nombre, descripcion, activo, TamX, TamY, posY, posX, id]);
+
+        if (rows.affectedRows > 0 ){
+            return res.status(200).json({"message":"Lote actualizado correctamente."});
+        }else {
+            return res.status(404).json({"message":"no se encontro el lote"});
+        }
+    }catch (error){
+        console.error(error)
+        return res.status(500).json({"message":"Error en el servidor"});
+    }
+}
+
+export const eliminarLote = async (req, res) => {
+    try{
+        const id = req.params.id_lote;
+        if(!id){
+            return res.status(400).json({"message":"Informacion invalida"});
+        }
+        const sql = `DELETE FROM Lotes WHERE id_lote = ?`;
+        const [rows] = await pool.query(sql, [id]);
+
+        if (rows.affectedRows > 0){
+            return res.status(200).json({"message":"Lote eliminado correctamente."});
+        }else {
+            return res.status(404).json({"message":"No se encontro el lote"})
+        }
+    }catch (error) {
+        console.error(error)
+        return res.status(500).json({"message":"Error en el servidor"});
     }
 }
--- src/routers/router.lotes.js
+++ src/routers/router.lotes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { listarLotes, registrarLote } from "../controllers/controller.lotes.js";
+import { listarLotes, verLote, registrarLote, actualizarLote, eliminarLote } from "../controllers/controller.lotes.js";
 const rutaLote = Router()
 
 rutaLote.get("/lotes", listarLotes);
+rutaLote.get("/lotes/:id_lote", verLote);
 rutaLote.post("/lotes",registrarLote);
+rutaLote.put("/lotes/:id_lote", actualizarLote);
+rutaLote.delete("/lotes/:id_lote",eliminarLote);
+
 
 export default rutaLote;
